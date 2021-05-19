ee01bb2fcc45e13c4cd9fb4942ca789d81d2f4a2
Ken Smith <42561470+kennethjsmith@users.noreply.github.com>
2021-05-19 16:51:04 -0700

    updates to skin class to assist in loading map icon

--- src/model/Skin.java
+++ src/model/Skin.java
@@ -11,40 +11,48 @@ import view.GameIcon;
 public class Skin {
 	// fields
 	private SkinType myType;
-	private Map<IconDirection, GameIcon[]> myImageIconMap;
+	private GameIcon myMapIcon;
+	private Map<IconDirection, GameIcon[]> myRoomIconMap;
 	//TODO: add front/rear facing icons
 	private static final int DEFAULT_SIZE = 100;
 	
 	
 	public Skin(SkinType theType){
     	myType = theType;
-        myImageIconMap = new HashMap<>();
+        myRoomIconMap = new HashMap<>();
         
-        GameIcon[] myLeftIcons = loadMyIcons(IconDirection.LEFT, myType);
-        GameIcon[] myRightIcons = loadMyIcons(IconDirection.RIGHT, myType);
-        myImageIconMap.put(IconDirection.LEFT, myLeftIcons);
-        myImageIconMap.put(IconDirection.RIGHT, myRightIcons);
+        GameIcon[] myLeftIcons = loadRoomIcons(IconDirection.LEFT, myType);
+        GameIcon[] myRightIcons = loadRoomIcons(IconDirection.RIGHT, myType);
+        myRoomIconMap.put(IconDirection.LEFT, myLeftIcons);
+        myRoomIconMap.put(IconDirection.RIGHT, myRightIcons);
+        
+        myMapIcon = loadMapIcon(theType);
 	}
 	
 	/*
 	 * Generates array of icons for a given icon direction and skin type.
 	 */
-	private GameIcon[] loadMyIcons(IconDirection theIconDirection, SkinType theSkinType) {
-		GameIcon[] inIcons = new GameIcon[5];
+	private GameIcon[] loadRoomIcons(IconDirection theIconDirection, SkinType theSkinType) {
+		GameIcon[] inRoomIcons = new GameIcon[5];
 		for (int i = 0; i <= 4; i++) { 
-		    String inFileName = getImageFileName(theIconDirection.getLabel(), theSkinType.getLabel(), i);
-		    inIcons[i] = new GameIcon(inFileName);
-		    inIcons[i].resize(DEFAULT_SIZE);
+		    String inFileName = getRoomIconFileName(theIconDirection.getLabel(), theSkinType.getLabel(), i);
+		    inRoomIcons[i] = new GameIcon(inFileName);
+		    inRoomIcons[i].resize(DEFAULT_SIZE);
 		}
-		return inIcons;
+		return inRoomIcons;
 	}
 
+	private GameIcon loadMapIcon(SkinType theSkinType) {
+		GameIcon inMapIcon = new GameIcon(getMapIconFileName(theSkinType.getLabel()));
+		return inMapIcon;
+	}
+	
 	/**
      * Builds a string representation of the objects image file name.
      * 
      * @return sb.toString() the string representation
      */
-    private String getImageFileName(String theDirectionLabel, String theSkinTypeLabel, int i) {
+    private String getRoomIconFileName(String theDirectionLabel, String theSkinTypeLabel, int i) {
     	
         StringBuilder sb = new StringBuilder();
         sb.append("src/icons/");
@@ -55,10 +63,21 @@ public class Skin {
         sb.append(".png");
   
         return sb.toString(); 
-    }		
+    }
+    
+    private String getMapIconFileName(String theSkinTypeLabel) {
+    	
+        StringBuilder sb = new StringBuilder();
+        sb.append("src/icons/");
+        sb.append(theSkinTypeLabel.toLowerCase());
+        sb.append("_map_icon");
+        sb.append(".png");
+  
+        return sb.toString(); 
+    }
 	
 	public GameIcon getIcon(IconDirection theIconDirection, int theStride) {
 		//System.out.println(myImageIconMap.get(theIconDirection)[theStride]);
-		return myImageIconMap.get(theIconDirection)[theStride];
+		return myRoomIconMap.get(theIconDirection)[theStride];
 	}
 }
