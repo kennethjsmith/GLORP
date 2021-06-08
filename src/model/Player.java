6786aa95b90686b64c0dcf8728f966a23e955b53
Ken Smith <42561470+kennethjsmith@users.noreply.github.com>
2021-06-07 22:26:22 -0700

    change player speed

--- src/model/Player.java
+++ src/model/Player.java
@@ -36,7 +36,7 @@ public class Player extends GamePiece implements Cloneable, Serializable{
 	private boolean isFixed;
 	private boolean isClone;
 
-	private static final int SPEED = 20;
+	private static final int SPEED = 10;
 	private static final int PLAYER_ROOM_ICON_SIZE = 100;
 	private static final Skin DEFAULT_SKIN = new Skin(SkinType.ALIEN);
 	
